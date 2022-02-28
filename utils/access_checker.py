8e0fdbb6e34c26a6633867a175cb438961ebac74
Abbot1227 <72700584+Abbot1227@users.noreply.github.com>
2022-02-28 15:49:35 +0600

    Update access_checker.py

--- utils/access_checker.py
+++ utils/access_checker.py
@@ -1,7 +1,7 @@
-ADMIN_ID = '2011969371'
+ADMIN_ID = ['SAMPLE_ID1', 'SAMPLE_ID2']
 
 def check_status(user_id: str) -> bool:
-	if user_id == ADMIN_ID:
+	if user_id in ADMIN_ID:
 		return True
 
 	else:
